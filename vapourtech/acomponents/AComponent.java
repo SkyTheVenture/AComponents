9ee342ed17da8591cfce58188c78de7a277d0e17
SkyTheVenture <maybeanother98@gmail.com>
2013-08-09 20:04:47 +0100

    Create AComponent.java

---
+++ vapourtech/acomponents/AComponent.java
@@ -0,0 +1,23 @@
+package vapourtech.acomponents;
+
+import java.awt.Graphics;
+
+public class AComponent {
+  
+	private int id = 0;
+	
+	public AComponent(){
+		
+	}
+	
+	public int getID(){
+		return id;
+	}
+	
+	public void draw(Graphics g){
+		
+	}
+	
+	
+
+}
