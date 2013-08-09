d4f9051f79846b9ba89016a9c79ceccb9c6548f9
SkyTheVenture <maybeanother98@gmail.com>
2013-08-09 20:12:36 +0100

    Update AComponent.java
    Added and compressed the code

--- vapourtech/acomponents/AComponent.java
+++ vapourtech/acomponents/AComponent.java
@@ -3,20 +3,28 @@ package vapourtech.acomponents;
 import java.awt.Graphics;
 
 public class AComponent {
-  
+	
+	/**
+	 * DO NOT CHANGE ANY CODE
+	 * DO NOT CALL THESE METHODS:
+	 * setId(Integer)
+	 * draw(Graphics)
+	 * OTHERWISE IT'S FINE
+	 * 
+	 * AComponent Created by Aero
+	 * (Removing this comment is fine!)
+	 */
+	
 	private int id = 0;
-	
-	public AComponent(){
-		
-	}
-	
-	public int getID(){
-		return id;
-	}
-	
-	public void draw(Graphics g){
-		
-	}
+	int x, y, width, height;
+	public AComponent(){}
+	public int getID(){ return id; }
+	public void setID(int a){ this.id = a; }
+	public void draw(Graphics g){}
+	public void setX(int x){ this.x = x;  }
+	public void setY(int y){ this.y = y; }
+	public void setWidth(int width){ this.width = width; }
+	public void setHeight(int height){ this.height = height; }
 	
 	
 
